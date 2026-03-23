508660a72f19017c2d5c4f175212ae2a1c132a18
manuelbunster-commits <manuel.bunster@relif.cl>
2026-03-23 11:21:42 -0300

    Add BCI logo image and use it in dashboard title

--- utils.py
+++ utils.py
@@ -86,7 +86,7 @@ def render_dashboard(bank_filter: str = None):
         if bank_filter:
             col_logo, col_title = st.columns([0.3, 5])
             with col_logo:
-                st.image("https://logo.clearbit.com/bci.cl", width=60)
+                st.image("bci_logo.png", width=80)
             with col_title:
                 st.title(bank_filter)
         else:
